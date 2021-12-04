30719e13db31cf35a50947a44e06953f40ef91f7
bmurase <biamurase@gmail.com>
2021-12-04 01:24:39 -0300

    extracting watched videos collection

--- src/Domain/Student/Student.php
+++ src/Domain/Student/Student.php
@@ -4,6 +4,7 @@ namespace Alura\Calisthenics\Domain\Student;
 
 use Alura\Calisthenics\Domain\Email\Email;
 use Alura\Calisthenics\Domain\Video\Video;
+use DateTimeImmutable;
 use DateTimeInterface;
 use Ds\Map;
 
@@ -11,7 +12,7 @@ class Student
 {
     private Email $email;
     private DateTimeInterface $bd;
-    private Map $watchedVideos;
+    private WatchedVideos $watchedVideos;
     private string $fName;
     private string $lName;
     public string $street;
@@ -23,7 +24,7 @@ class Student
 
     public function __construct(Email $email, DateTimeInterface $bd, string $fName, string $lName, string $street, string $number, string $province, string $city, string $state, string $country)
     {
-        $this->watchedVideos = new Map();
+        $this->watchedVideos = new WatchedVideos();
         $this->email = $email;
         $this->bd = $bd;
         $this->fName = $fName;
@@ -53,7 +54,7 @@ class Student
 
     public function watch(Video $video, DateTimeInterface $date)
     {
-        $this->watchedVideos->put($video, $date);
+        $this->watchedVideos->add($video, $date);
     }
 
     public function hasAccess(): bool
@@ -67,10 +68,8 @@ class Student
 
     private function firstVideoWasWatchedLessThan90DaysAgo(): bool
     {
-        $this->watchedVideos->sort(fn(DateTimeInterface $dateA, DateTimeInterface $dateB) => $dateA <=> $dateB);
-        /** @var DateTimeInterface $firstDate */
-        $firstDate = $this->watchedVideos->first()->value;
-        $today = new \DateTimeImmutable();
+        $firstDate = $this->watchedVideos->dateOfFirstVideo();
+        $today = new DateTimeImmutable();
 
         return $firstDate->diff($today)->days < 90;
     }
